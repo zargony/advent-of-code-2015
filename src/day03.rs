c0e2b5dab20657c003da90265b0b4caabdde01b7
Andreas Neuhaus <zargony@zargony.com>
2016-10-25 03:00:16 +0200

    Clean up and complete day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,95 +1,127 @@
-// use std::collections::HashMap;
+use std::collections::HashSet;
 use std::str;
 
-#[allow(dead_code)]     // XXX
-pub struct Visits<'a> {
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Position {
     x: i32,
     y: i32,
-    iter: &'a Iterator<Item=char>,
 }
 
-// impl<'a> Iterator for Visits<'a> {
-//     type Item = (i32, i32);
+impl Position {
+    fn new() -> Position {
+        Position { x: 0, y: 0 }
+    }
 
-//     fn next(&mut self) -> Option<(i32, i32)> {
-//         self.iter.next().map(|ch| {
-//             match ch {
-//                 '^' => self.y += 1,
-//                 'v' => self.y -= 1,
-//                 '>' => self.x += 1,
-//                 '<' => self.x -= 1,
-//                 _ => (),
-//             }
-//             (self.x, self.y)
-//         })
-//     }
-// }
+    fn step(&mut self, step: char) {
+        match step {
+            '^' => self.y += 1,
+            'v' => self.y -= 1,
+            '>' => self.x += 1,
+            '<' => self.x -= 1,
+            _ => panic!("invalid character"),
+        }
+    }
+}
 
-#[allow(dead_code)]     // XXX
-pub struct Instructions<'a> {
+pub struct Visits {
+    position: Position,
+    visited: HashSet<Position>,
+}
+
+impl Visits {
+    fn new() -> Visits {
+        let mut visited = HashSet::new();
+        visited.insert(Position::new());
+        Visits { position: Position::new(), visited: visited }
+    }
+
+    fn step(&mut self, step: char) {
+        self.position.step(step);
+        self.visited.insert(self.position);
+    }
+
+    fn unique(&self) -> usize {
+        self.visited.len()
+    }
+}
+
+pub struct DuoVisits {
+    position1: Position,
+    position2: Position,
+    visited: HashSet<Position>,
+    second: bool,
+}
+
+impl DuoVisits {
+    fn new() -> DuoVisits {
+        let mut visited = HashSet::new();
+        visited.insert(Position::new());
+        DuoVisits { position1: Position::new(), position2: Position::new(), visited: visited, second: false }
+    }
+
+    fn step(&mut self, step: char) {
+        if self.second {
+            self.position1.step(step);
+            self.visited.insert(self.position1);
+        } else {
+            self.position2.step(step);
+            self.visited.insert(self.position2);
+        }
+        self.second = !self.second;
+    }
+
+    fn unique(&self) -> usize {
+        self.visited.len()
+    }
+}
+
+pub struct Directions<'a> {
     steps: &'a str,
 }
 
-impl<'a> Instructions<'a> {
-    pub fn new(steps: &str) -> Instructions {
-        Instructions { steps: steps }
+impl<'a> Directions<'a> {
+    fn new(steps: &str) -> Directions {
+        Directions { steps: steps }
     }
 
-    // pub fn visits(&self) -> Visits {
-    //     Visits { x: 0, y: 0, iter: &self.steps.chars() }
-    // }
-
-    // pub fn visits_with_robo(&self) -> (Visits, Visits) {
-    //     let odd_steps  = self.steps.chars().enumerate().filter_map(|(i, ch)| if i % 2 != 0 { Some(ch) } else { None } );
-    //     let even_steps = self.steps.chars().enumerate().filter_map(|(i, ch)| if i % 2 == 0 { Some(ch) } else { None } );
-    //     (Visits { x: 0, y: 0, iter: &odd_steps }, Visits { x: 0, y: 0, iter: &even_steps })
-    // }
-
-    pub fn unique_visits(&self) -> usize {
-        // let mut houses = HashMap::new();
-        // houses.insert((0, 0), 1);
-        // for coord in self.visits() {
-        //     let counter = houses.entry(coord).or_insert(0);
-        //     *counter += 1;
-        // }
-        // houses.len()
-        0
+    fn unique_visits(&self) -> usize {
+        let mut visits = Visits::new();
+        for step in self.steps.chars() {
+            visits.step(step);
+        }
+        visits.unique()
     }
 
-    pub fn unique_visits_with_robo(&self) -> usize {
-        // let mut houses = HashMap::new();
-        // houses.insert((0, 0), 2);
-        // let (mut santa_visits, mut robo_visits) = self.visits().enumerate().partition(|&(i, _)| i % 2 == 0);
-        // for coord in santa_visits {
-        //     let counter = houses.entry(coord).or_insert(0);
-        //     *counter += 1;
-        // }
-        // houses.len()
-        0
+    fn unique_visits_with_robo(&self) -> usize {
+        let mut visits = DuoVisits::new();
+        for step in self.steps.chars() {
+            visits.step(step);
+        }
+        visits.unique()
     }
 }
 
 fn main() {
-    let instructions = Instructions::new(include_str!("day03.txt"));
-    println!("Unique visits: {}", instructions.unique_visits());
-    println!("Unique visits with robo: {}", instructions.unique_visits_with_robo());
+    let directions = Directions::new(include_str!("day03.txt"));
+    println!("Unique visits: {}", directions.unique_visits());
+    println!("Unique visits with robo: {}", directions.unique_visits_with_robo());
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
-    // #[test]
-    // fn unique_visits() {
-    //     assert_eq!(Instructions::new(">"         ).unique_visits(), 2);
-    //     assert_eq!(Instructions::new("^>v<"      ).unique_visits(), 4);
-    //     assert_eq!(Instructions::new("^v^v^v^v^v").unique_visits(), 2);
-    // }
+    #[test]
+    fn unique_visits() {
+        assert_eq!(Directions::new(">"         ).unique_visits(), 2);
+        assert_eq!(Directions::new("^>v<"      ).unique_visits(), 4);
+        assert_eq!(Directions::new("^v^v^v^v^v").unique_visits(), 2);
+    }
 
-    // #[test]
-    // fn unique_visits_with_robo() {
-    //     assert_eq!(Instructions::new("^v"        ).unique_visits_with_robo(),  3);
-    //     assert_eq!(Instructions::new("^>v<"      ).unique_visits_with_robo(),  3);
-    //     assert_eq!(Instructions::new("^v^v^v^v^v").unique_visits_with_robo(), 11);
-    // }
+    #[test]
+    fn unique_visits_with_robo() {
+        assert_eq!(Directions::new("^v"        ).unique_visits_with_robo(),  3);
+        assert_eq!(Directions::new("^>v<"      ).unique_visits_with_robo(),  3);
+        assert_eq!(Directions::new("^v^v^v^v^v").unique_visits_with_robo(), 11);
+    }
 }
