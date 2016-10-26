80a736c681fbda4e140ebaac5497960428cddbc4
Andreas Neuhaus <zargony@zargony.com>
2016-10-26 17:24:11 +0200

    Day 5 part two

--- src/day05.rs
+++ src/day05.rs
@@ -1,32 +1,75 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
+extern crate onig;
 
-use regex::Regex;
+use onig::Regex;
+
+pub trait Matcher {
+    fn is_match(&self, s: &str) -> bool;
+}
+
+pub struct OldNicenessMatcher {
+    re1: Regex,
+    re2: Regex,
+    re3: Regex,
+}
+
+impl OldNicenessMatcher {
+    fn new() -> OldNicenessMatcher {
+        OldNicenessMatcher {
+            re1: Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap(),
+            re2: Regex::new(r"([\w])\1").unwrap(),
+            re3: Regex::new(r"(ab|cd|pq|xy)").unwrap(),
+        }
+    }
+}
+
+impl Matcher for OldNicenessMatcher {
+    fn is_match(&self, s: &str) -> bool {
+        self.re1.find(s).is_some() && self.re2.find(s).is_some() && !self.re3.find(s).is_some()
+    }
+}
+
+pub struct NewNicenessMatcher {
+    re1: Regex,
+    re2: Regex,
+}
+
+impl NewNicenessMatcher {
+    fn new() -> NewNicenessMatcher {
+        NewNicenessMatcher {
+            re1: Regex::new(r"(\w\w).*\1").unwrap(),
+            re2: Regex::new(r"(\w)\w\1").unwrap(),
+        }
+    }
+}
+
+impl Matcher for NewNicenessMatcher {
+    fn is_match(&self, s: &str) -> bool {
+        self.re1.find(s).is_some() && self.re2.find(s).is_some()
+    }
+}
 
 pub trait SantaStringExtension {
-    fn is_nice(&self) -> bool;
-    fn is_naughty(&self) -> bool { !self.is_nice() }
-    fn nice_lines(&self) -> usize;
+    fn is_nice<M: Matcher>(&self, m: &M) -> bool;
+    fn is_naughty<M: Matcher>(&self, m: &M) -> bool { !self.is_nice(m) }
+    fn nice_lines<M: Matcher>(&self, m: &M) -> usize;
 }
 
 impl<'a> SantaStringExtension for &'a str {
-    fn is_nice(&self) -> bool {
-        lazy_static! {
-            static ref RE1: Regex = Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap();
-            static ref RE2: Regex = Regex::new(r"(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)").unwrap();
-            static ref RE3: Regex = Regex::new(r"(ab|cd|pq|xy)").unwrap();
-        }
-        RE1.is_match(self) && RE2.is_match(self) && !RE3.is_match(self)
+    fn is_nice<M: Matcher>(&self, m: &M) -> bool {
+        m.is_match(self)
     }
 
-    fn nice_lines(&self) -> usize {
-        self.lines().filter(|line| line.is_nice()).count()
+    fn nice_lines<M: Matcher>(&self, m: &M) -> usize {
+        self.lines().filter(|line| line.is_nice(m)).count()
     }
 }
 
 fn main() {
-    println!("Nice strings: {}", include_str!("day05.txt").nice_lines());
+    let input = include_str!("day05.txt");
+    let m = OldNicenessMatcher::new();
+    println!("Nice strings (old rules): {}", input.lines().filter(|line| m.is_match(line)).count());
+    let m = NewNicenessMatcher::new();
+    println!("Nice strings (new rules): {}", input.lines().filter(|line| m.is_match(line)).count());
 }
 
 #[cfg(test)]
@@ -34,27 +77,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ugknbfddgicrmopn_is_nice() {
-        assert!("ugknbfddgicrmopn".is_nice());
+    fn old_niceness() {
+        let m = OldNicenessMatcher::new();
+        assert!("ugknbfddgicrmopn".is_nice(&m));
+        assert!("aaa".is_nice(&m));
+        assert!("jchzalrnumimnmhp".is_naughty(&m));
+        assert!("haegwjzuvuyypxyu".is_naughty(&m));
+        assert!("dvszwmarrgswjxmb".is_naughty(&m));
     }
 
     #[test]
-    fn aaa_is_nice() {
-        assert!("aaa".is_nice());
-    }
-
-    #[test]
-    fn jchzalrnumimnmhp_is_naughty() {
-        assert!("jchzalrnumimnmhp".is_naughty());
-    }
-
-    #[test]
-    fn haegwjzuvuyypxyu_is_naughty() {
-        assert!("haegwjzuvuyypxyu".is_naughty());
-    }
-
-    #[test]
-    fn dvszwmarrgswjxmb_is_naughty() {
-        assert!("dvszwmarrgswjxmb".is_naughty());
+    fn new_niceness() {
+        let m = NewNicenessMatcher::new();
+        assert!("qjhvhtzxzqqjkmpb".is_nice(&m));
+        assert!("xxyxx".is_nice(&m));
+        assert!("uurcxstgmygtbstg".is_naughty(&m));
+        assert!("ieodomkazucvgmuy".is_naughty(&m));
     }
 }
