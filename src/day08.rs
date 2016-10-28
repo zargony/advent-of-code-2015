5623a58cd22ad3d53e0018c2cc93a17a9e073e9d
Andreas Neuhaus <zargony@zargony.com>
2016-10-28 18:53:52 +0200

    Day 8 part two

--- src/day08.rs
+++ src/day08.rs
@@ -15,12 +15,31 @@ pub fn raw_and_unescaped_len(s: &str) -> (usize, usize) {
     (raw_len, raw_len - 2 - esc_size + esc_count)
 }
 
-fn main() {
-    let extra_chars = include_str!("day08.txt").lines().fold(0, |extra_chars, line| {
+pub fn extra_chars_unescaped(text: &str) -> usize {
+    text.lines().fold(0, |extra_chars, line| {
         let (raw_len, unescaped_len) = raw_and_unescaped_len(line);
         extra_chars + (raw_len - unescaped_len)
-    });
-    println!("Number of extra characters: {}", extra_chars);
+    })
+}
+
+pub fn raw_and_reescaped_len(s: &str) -> (usize, usize) {
+    let raw_len = s.len();
+    let re = Regex::new(r#"[\\"]"#).unwrap();
+    let esc_count = re.find_iter(s).count();
+    (raw_len, raw_len + 2 + esc_count)
+}
+
+pub fn extra_chars_reescaped(text: &str) -> usize {
+    text.lines().fold(0, |extra_chars, line| {
+        let (raw_len, reescaped_len) = raw_and_reescaped_len(line);
+        extra_chars + (reescaped_len - raw_len)
+    })
+}
+
+fn main() {
+    let input = include_str!("day08.txt");
+    println!("Number of extra characters: {}", extra_chars_unescaped(input));
+    println!("Number of extra characters re-escaping: {}", extra_chars_reescaped(input));
 }
 
 #[cfg(test)]
@@ -28,10 +47,28 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn unescaped() {
         assert_eq!(raw_and_unescaped_len(r#""""#), (2, 0));
         assert_eq!(raw_and_unescaped_len(r#""abc""#), (5, 3));
         assert_eq!(raw_and_unescaped_len(r#""aaa\"aaa""#), (10, 7));
         assert_eq!(raw_and_unescaped_len(r#""\x27""#), (6, 1));
+    }
+
+    #[test]
+    fn unescaped_count() {
+        assert_eq!(extra_chars_unescaped("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\""), 12);
+    }
+
+    #[test]
+    fn reescaped() {
+        assert_eq!(raw_and_reescaped_len(r#""""#), (2, 6));
+        assert_eq!(raw_and_reescaped_len(r#""abc""#), (5, 9));
+        assert_eq!(raw_and_reescaped_len(r#""aaa\"aaa""#), (10, 16));
+        assert_eq!(raw_and_reescaped_len(r#""\x27""#), (6, 11));
+    }
+
+    #[test]
+    fn reescaped_count() {
+        assert_eq!(extra_chars_reescaped("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\""), 19);
     }
 }
