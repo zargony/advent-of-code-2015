c2e33dd87fa5f8f5aa632a8f3fe34fb801838b6a
Andreas Neuhaus <zargony@zargony.com>
2016-10-28 02:32:23 +0200

    Day 6 part two

--- src/day06.rs
+++ src/day06.rs
@@ -4,7 +4,7 @@ extern crate nom;
 use std::str::{self, FromStr};
 use nom::{IResult, digit, space};
 
-named!(number <usize>,
+named!(number(&[u8]) -> usize,
     map_res!(
         map_res!(
             digit,
@@ -21,7 +21,7 @@ pub enum Command {
     Toggle,
 }
 
-named!(command <Command>,
+named!(command(&[u8]) -> Command,
     alt!(
         value!(Command::TurnOn, tag!("turn on")) |
         value!(Command::TurnOff, tag!("turn off")) |
@@ -38,7 +38,7 @@ pub struct Instruction {
     y2: usize,
 }
 
-named!(instruction <Instruction>,
+named!(instruction(&[u8]) -> Instruction,
     chain!(
         cmd: command ~
         space ~
@@ -55,27 +55,62 @@ named!(instruction <Instruction>,
     )
 );
 
-pub struct Grid([[bool; 1000]; 1000]);
+pub trait Light {
+    fn switch(&mut self, cmd: &Command);
+    fn brightness(&self) -> usize;
+}
 
-impl Grid {
-    fn new() -> Grid {
-        Grid([[false; 1000]; 1000])
+impl Light for bool {
+    fn switch(&mut self, cmd: &Command) {
+        match cmd {
+            &Command::TurnOn => *self = true,
+            &Command::TurnOff => *self = false,
+            &Command::Toggle => *self = !*self,
+        }
     }
 
-    fn count(&self) -> usize {
+    fn brightness(&self) -> usize {
+        match *self {
+            false => 0,
+            true => 1,
+        }
+    }
+}
+
+impl Light for u8 {
+    fn switch(&mut self, cmd: &Command) {
+        match cmd {
+            &Command::TurnOn => *self += 1,
+            &Command::TurnOff if *self > 0 => *self -= 1,
+            &Command::TurnOff => (),
+            &Command::Toggle => *self += 2,
+        }
+    }
+
+    fn brightness(&self) -> usize {
+        *self as usize
+    }
+}
+
+pub struct Grid<T>([[T; 1000]; 1000]);
+
+impl<T: Copy> Grid<T> {
+    fn new(default: T) -> Grid<T> {
+        Grid([[default; 1000]; 1000])
+    }
+}
+
+impl<T: Light> Grid<T> {
+    fn brightness(&self) -> usize {
         self.0.iter().map(|line|
-            line.iter().filter(|light| **light).count()
+            line.iter().map(|light| light.brightness()).sum::<usize>()
         ).sum()
     }
 
     fn change(&mut self, ins: &Instruction) {
         for y in ins.y1 .. ins.y2 + 1 {
             for x in ins.x1 .. ins.x2 + 1 {
-                match ins.command {
-                    Command::TurnOn => self.0[y][x] = true,
-                    Command::TurnOff => self.0[y][x] = false,
-                    Command::Toggle => self.0[y][x] = !self.0[y][x],
-                }
+                self.0[y][x].switch(&ins.command);
             }
         }
     }
@@ -91,9 +126,13 @@ impl Grid {
 }
 
 fn main() {
-    let mut grid = Grid::new();
-    grid.run_instructions(include_str!("day06.txt"));
-    println!("Number of lit lights: {}", grid.count());
+    let input = include_str!("day06.txt");
+    let mut grid = Grid::new(false);
+    grid.run_instructions(input);
+    println!("Number of lit lights: {}", grid.brightness());
+    let mut grid = Grid::new(0);
+    grid.run_instructions(input);
+    println!("Total brightness of all lights: {}", grid.brightness());
 }
 
 #[cfg(test)]
@@ -102,20 +141,30 @@ mod tests {
 
     #[test]
     fn switching_lights() {
-        let mut grid = Grid::new();
-        assert_eq!(grid.count(), 0);
+        let mut grid = Grid::new(false);
+        assert_eq!(grid.brightness(), 0);
         grid.change(&Instruction { command: Command::TurnOn, x1: 0, y1: 0, x2: 999, y2: 999 });
-        assert_eq!(grid.count(), 1_000_000);
+        assert_eq!(grid.brightness(), 1_000_000);
         grid.change(&Instruction { command: Command::Toggle, x1: 0, y1: 0, x2: 999, y2: 0 });
-        assert_eq!(grid.count(), 999_000);
+        assert_eq!(grid.brightness(), 999_000);
         grid.change(&Instruction { command: Command::TurnOff, x1: 499, y1: 499, x2: 500, y2: 500 });
-        assert_eq!(grid.count(), 998_996);
+        assert_eq!(grid.brightness(), 998_996);
+    }
+
+    #[test]
+    fn dimming_lights() {
+        let mut grid = Grid::new(0u8);
+        assert_eq!(grid.brightness(), 0);
+        grid.change(&Instruction { command: Command::TurnOn, x1: 0, y1: 0, x2: 0, y2: 0 });
+        assert_eq!(grid.brightness(), 1);
+        grid.change(&Instruction { command: Command::Toggle, x1: 0, y1: 0, x2: 999, y2: 999 });
+        assert_eq!(grid.brightness(), 2_000_001);
     }
 
     #[test]
     fn running_instructions() {
-        let mut grid = Grid::new();
+        let mut grid = Grid::new(false);
         grid.run_instructions("turn on 0,0 through 999,999\ntoggle 0,0 through 999,0\nturn off 499,499 through 500,500");
-        assert_eq!(grid.count(), 998_996);
+        assert_eq!(grid.brightness(), 998_996);
     }
 }
